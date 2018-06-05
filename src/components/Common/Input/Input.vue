ff7c958494f86a866f9659174a2c42e8181f3767
accAyooo <shixiangyu01@icloud.com>
2018-06-05 22:58:54 +0800

    login

---
+++ src/components/Common/Input/Input.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="form_input">
+    <!--<img v-if="iconUrl" src="{{iconUrl}}" alt="">-->
+    <input type="text" name="{{formName}}" />
+    <!--{{rightDiv}}-->
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['formName']
+}
+</script>
+
+<style lang="less" scoped>
+  @import "style";
+</style>
--- src/components/Login/Login.vue
+++ src/components/Login/Login.vue
@@ -1,13 +1,24 @@
 <template>
   <div class="login_page">
-    login
+    <div class="loginlogo">
+      <img class="logo" src="http://b-lemon.heiyanimg.com/_res/css/zhangwen/mobile/img/zwlogo@3x.png" alt="">
+    </div>
+    <form-input :formName="formName"/>
   </div>
 </template>
 
 <script>
-export default {}
+import FormInput from '../Common/Input/Input'
+export default {
+  data: () => ({
+    formName: 'form'
+  }),
+  components: {
+    FormInput
+  }
+}
 </script>
 
 <style lang="less">
-
+  @import "style";
 </style>
