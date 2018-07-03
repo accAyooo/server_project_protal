f3fb3a3a4c71a8fa6b66236c9c705a35f139aa2e
shixiangyu216 <773681606@qq.com>
2018-07-03 11:30:41 +0800

    smallHeader

---
+++ src/components/Header/GuideNav/GuideNav.vue
@@ -0,0 +1,30 @@
+<template>
+<div class="guide" v-if="isShow">
+  <div class="guide-content">
+    <nav class="guide-nav">
+      <router-link to="/" class="guide-nav-a">
+        <i class="icon icon-home"></i>
+        <span class="guide-nav-h">首页</span>
+      </router-link>
+      <router-link to="/accounts/people" class="guide-nav-a">
+        <i class="icon icon-user"></i>
+        <span class="guide-nav-h">账户</span>
+      </router-link>
+    </nav>
+    <div class="guide-footer">
+    </div>
+  </div>
+  <div class="close-guide"></div>
+</div>
+</template>
+
+<script>
+export default {
+  props: {
+    isShow: {
+      type: Boolean,
+      default: true
+    }
+  }
+}
+</script>
--- src/components/Header/SmallHeader/SmallHeader.vue
+++ src/components/Header/SmallHeader/SmallHeader.vue
@@ -1,18 +1,30 @@
 <template>
 <div class="small-header">
   <div class="left">
-    <router-link @click.stop.prevent class="back" v-bind:to="backUrl"></router-link>
+    <router-link v-if="backUrl !== ''" class="back" v-bind:to="backUrl"></router-link>
+    <a class="back" @click="$router.go(-1)" v-else></a>
     <span class="name">{{ title }}</span>
   </div>
   <div class="right">
     <router-link class="search" to="/search"></router-link>
-    <a class="open-guide" @click="openGuide()"></a>
+    <a class="open-guide" @click="switchGuide()"></a>
   </div>
+  <guide-nav :isShow="isGuideNavShow"></guide-nav>
 </div>
 </template>
 
 <script>
+import GuideNav from 'components/Header/GuideNav/GuideNav'
+
 export default {
+  components: {
+    GuideNav
+  },
+  data () {
+    return {
+      isGuideNavShow: false
+    }
+  },
   props: {
     title: {
       type: String,
@@ -21,6 +33,11 @@ export default {
     backUrl: {
       type: String,
       default: ''
+    }
+  },
+  methods: {
+    switchGuide: function () {
+      this.isGuideNavShow = !this.isGuideNavShow
     }
   }
 }
